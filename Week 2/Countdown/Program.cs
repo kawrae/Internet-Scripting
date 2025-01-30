4c7484c97ca6d4f9fe92b02fc1fd77c092c3e3a4
kawrae <coreyblack@hotmail.co.uk>
2025-01-30 01:29:17 +0000

    Exercise 3 & 4 - Week 2

---
+++ Week 2/Countdown/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Real-Time Countdown");
+
+for (int i = 10; i > 0; i--)
+{
+    Console.WriteLine(i);
+    System.Threading.Thread.Sleep(1000);
+}
+
+Console.WriteLine("Boom");
+
+Console.Write("Press any key to exit...");
+Console.ReadKey();
---
+++ Week 2/RandomSum/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Random Sum");
+
+Random rnd = new Random();
+
+int num1 = rnd.Next(1, 21);
+int num2 = rnd.Next(1, 21);
+int correctAnswer = num1 + num2;
+
+Console.WriteLine($"What is {num1} + {num2}");
+
+int userAnswer = -1;
+
+while (userAnswer != correctAnswer)
+{
+    Console.Write("Enter your answer: ");
+    userAnswer = int.Parse(Console.ReadLine());
+
+    if (userAnswer == correctAnswer)
+    {
+        Console.WriteLine("Correct answer.");
+    }
+    else
+    {
+        Console.WriteLine("Incorrect try again.");
+    }
+}
+
+Console.Write("Press any key to exit...");
+Console.ReadKey();
