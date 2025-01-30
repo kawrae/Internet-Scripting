85c4870c8b67b76471f11bb30a8182d222ac89b6
kawrae <coreyblack@hotmail.co.uk>
2025-01-30 00:36:01 +0000

    Exercise 1 & 2 - Week 2

---
+++ Week 2/DrawRectangle/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Draw Rectangle");
+
+Console.WriteLine($"Number of Rows: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Number of columns: ");
+int columns = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Press any key to exit...");
+Console.ReadKey();
---
+++ Week 2/Temperature/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Temperature Celsius to Fahrenheit");
+
+Console.WriteLine($"Enter temperature in degrees Centigrade: ");
+Double celsius = Double.Parse(Console.ReadLine());
+
+Double fahrenheit = (1.8 * celsius) + 32;
+
+Console.WriteLine($"{celsius}°C is equivelant to {fahrenheit}°F");
+
+Console.Write("Enter any key to exit...");
+Console.ReadKey();
